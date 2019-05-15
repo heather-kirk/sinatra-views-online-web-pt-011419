28a2cd1be274e6d6fd9bb2c1ca470258fd6a6a06
Heather Kirk <hskirk@gmail.com>
2019-05-15 03:35:08 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,12 +2,21 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+<<<<<<< HEAD
+=======
+	get '/' do
+		"<h1> Hello World </h1>"
+	end
+>>>>>>> 4e5f4fb77355e3a01e7bbb6821c887a4d5412f81
 	
 	get '/' do 
 	  erb :index 
 	end 
+<<<<<<< HEAD
 	
 	get '/info' do 
 	  erb :info 
 	end 
+=======
+>>>>>>> 4e5f4fb77355e3a01e7bbb6821c887a4d5412f81
 end
